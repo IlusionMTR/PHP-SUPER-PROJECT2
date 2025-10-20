595840cdcc7d84e66885460a27fa9a5ee528f8a7
Bruno Ilusion <154312537+IlusionMTR@users.noreply.github.com>
2025-10-20 10:11:31 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPEhtml>
+<htmllang="pt-br">
+<head>
+<metacharset="UTF-8">
+<title>Página Inicial</title>
+</head>
+<body>
+<h2>Login</h2>
+<form method="POST"action="login.php">
+Usuário: <input type="text"name="usuario"><br>
+Senha: <input type="password"name="senha"><br>
+
+<select name="tema">
+<option value="claro">Claro</option>
+<option value="escuro">Escuro</option>
+</select><br>
+
+<input type="submit"value="Entrar">
+</form>
+
+</body>
+</html>
